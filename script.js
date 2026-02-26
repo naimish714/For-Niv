496223473774082a59f5643fcd55084d2d4d089f
naimish714 <naimish714@gmail.com>
2026-02-27 03:51:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ function nextImage() {
 }
 
 let messages = [
-  "Couldn't even have guessed what laid in front of us!",
-    "Hahahaha, the first time we took a picture together intentionally",
+  "Couldn't even have guessed what laid in front of us, when we first started out",
+    "Time feels kinder with you",
     "I feel blessed to see and be part of this journey with you",
     "I love waking up knowing that you are mine",
     "I’m proud of the the person you are and I miss you so damn much",
     "I would 100% become a demon-king and roam the world in search for you for 800 years",
+    "If I had been told the future held you, I would have walked toward it without hesitation.",
     "I wish you were here with me and we could go out again",
     "I can't wait to see you again, and start our life together",
     "I love you so much and I miss you so so much. Also my favourite pictures of us!"
@@ -42,5 +43,6 @@ function surprise() {
     alert("No matter the distance, I am yours. ❤️");
 
 }
+
 
 
